6fb73d7155932513ee779e89580195af9461d99e
GabrielAs15 <bielsantos200510@gmail.com>
2022-10-25 16:34:23 -0300

    atividade notas

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Acai from './pages/Prova1/index.js' 
+import Acai from './pages/Prova1/index.js'
 import Salario from './pages/Prova2/index.js'
 import Orcamento from './pages/prova3/index.js'
 import Array from './pages/Prova4/index.js'
 import Cafe from './pages/Prova5/index.js'
+import Notas from './pages/prova6/index.js'
 
-export default function SiteRoutes(){
-    return(
+export default function SiteRoutes() {
+    return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Acai />} />
                 <Route path='/salario' element={<Salario />} />
                 <Route path='/orcamento' element={<Orcamento />} />
-                <Route path='/contagem' element={<Array/>} />
-                <Route path='/cafe' element={<Cafe/>} />
-            </Routes>
-        </BrowserRouter>
+                <Route path='/contagem' element={<Array />} />
+                <Route path='/cafe' element={<Cafe />} />
+                <Route path='/nota' element={<Notas/>}/>
+        </Routes>
+        </BrowserRouter >
     )
 }
